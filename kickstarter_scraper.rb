67558bcc32f5c5af248a9a12995fd2b1bf946948
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-18 19:40:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,10 +7,10 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = kickstarter.css("li.project.grid_4")
-  #title = projects.css("h2.bbcard_name strong a").text
-  #image = projects.css("div.project-thumbnail a img").attribute("src").value
-  #description = projects.css("p.bbcard_blurb").text
-  location = projects.css("span.location-name").text
+  #title = project.css("h2.bbcard_name strong a").text
+  #image = project.css("div.project-thumbnail a img").attribute("src").value
+  #description = projectscss("p.bbcard_blurb").text
+  location = projects.first.css("span.location-name").text
   binding.pry
 end
 
