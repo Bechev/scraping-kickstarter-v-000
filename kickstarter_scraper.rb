a382dc56fd1222c6942d98ad17e47eca2205fed4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-18 19:21:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,14 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  projects = kickstarter.css("li.project.grid_4")
+  first_project = projects.first.css("h2.bbcard_name").text
+  binding.pry
 end
+
+create_project_hash
